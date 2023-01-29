7d88d6766caae5baaf63e2b20365150f95b95852
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-01-29 13:20:20 -0500

    create
    create

---
+++ scripts/ps_1.R
@@ -0,0 +1,36 @@
+## PROBLEM SET 1 ##
+
+# (2) Data description
+
+
+
+# (3) Age-wage profile
+
+
+
+# (4) The gender earnings GAP
+
+
+#(a) Begin by estimating and discussing the unconditional wage gap:
+
+
+#(b) Equal Pay for Equal Work?
+
+
+#(c) Peak ages
+
+
+
+#(5) Predicting earnings
+
+
+#(a) Split the sample into two:
+
+
+#(b) Report and compare the predictive performance
+
+
+#(c) Discussion of the results
+
+
+#(d) LOOCV.F
